91308fc5a7878a3c089664828d57cc25b2397a14
jorge riquelme <jorge.riquelme.z@gmail.com>
2025-01-12 23:43:17 -0300

    correcciones de codigo y mensajes de alerta

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,45 +22,37 @@ const api = async () => {
 };
 
 const selectOptions = async () => {
-  const data = await api();
-  if (divisaSeleccionada === "dolar") {
-    return data.dolar.valor;
-  } else if (divisaSeleccionada === "euro") {
-    return data.euro.valor;
-  } else if (divisaSeleccionada === "uf") {
-    return data.uf.valor;
-  } else {
-    console.log("no identificado");
+  try {
+    const data = await api();
+    if (divisaSeleccionada === "dolar") {
+      return data.dolar.valor;
+    } else if (divisaSeleccionada === "euro") {
+      return data.euro.valor;
+    } else if (divisaSeleccionada === "uf") {
+      return data.uf.valor;
+    } else {
+      resultadoDiv.textContent = "Ingrese moneda válida.";
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 buscarButton.addEventListener("click", async () => {
-  const cantidad = parseFloat(cantidadInput.value);
-  if (isNaN(cantidad)) {
-    resultadoDiv.textContent = "Ingrese una cantidad válida.";
-    return;
-  }
-  const moneda = await selectOptions();
-  if (moneda) {
-    const resultado = cantidad / moneda;
-    resultadoDiv.textContent = `Resultado: ${resultado.toFixed(2)}`;
+  try {
+    const cantidad = parseFloat(cantidadInput.value);
+    if (isNaN(cantidad)) {
+      resultadoDiv.textContent = "Ingrese una cantidad válida.";
+      return;
+    }
+    const moneda = await selectOptions();
+    if (moneda) {
+      const resultado = cantidad / moneda;
+      resultadoDiv.textContent = `Resultado: ${resultado.toFixed(2)}`;
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
-// buscarButton.addEventListener("click", () => {
-//   const cantidad = parseFloat(cantidadInput.value);
-//   const divisa = divisaSelect.value;
-
-// //Se busca el valor en la api
-
-//   //Aqui se genera el resultado al tener los valores de la API mas lo tecleado
-//   const tipoDeCambio = tiposDeCambio[divisa];
-
-//   if (isNaN(cantidad)) {
-//     resultadoDiv.textContent = "Ingrese una cantidad válida.";
-//     return;
-// }
-// const resultado = cantidad / tipoDeCambio;
-// resultadoDiv.textContent = `Resultado: ${resultado.toFixed(2)}`;
-
-// });
+// Codigo para graficar en pantalla el historico de las monedas
